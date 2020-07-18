81c460d1d882ff22b76c0680c698eff2ad55deae
flsaiful <tusharimransaif@gmail.com>
2020-07-18 08:03:37 +0600

    first

---
+++ assignment.js
@@ -0,0 +1,62 @@
+
+/*
+feet to mile
+function feetToMile(feet){
+
+    var mile = feet/5280;
+    return mile;
+
+
+}
+var result = feetToMile(31680);
+console.log(result);
+feet to mile
+*/
+
+
+
+/*
+function fruitCalculator(mango, apple){
+
+    var mangoCount = mango * 4;
+    var appleCount = apple * 4;
+    var totalFruit = mangoCount + appleCount;
+    return totalFruit;
+
+
+}
+var fruitResult = fruitCalculator(5, 7);
+console.log(fruitResult);
+*/
+
+
+
+//wood calculator
+function woodCalculator(table, chair, bed){
+
+    var tableCount = table * 1;
+    var ChairCount = chair * 3;
+    var bedCount = bed * 5;
+    var totalWood = tableCount + ChairCount + bedCount;
+    return totalWood;
+
+
+}
+var woodresult = woodCalculator(4, 14, 12);
+console.log(woodresult);
+
+
+
+
+//brickCalculator
+
+function brickCalculator(eitFor7Floor, eitFor16Floor, eitFor30Floor){
+ var floor7 = eitFor7Floor *1000;
+ var floor16 = eitFor16Floor *1000;
+ var floor30 = eitFor30Floor *1000;
+ var totalit = floor7 + floor16 +floor30;
+ return totalit;
+
+}
+var totalresult = brickCalculator(15, 12, 10);
+console.log(totalresult);
